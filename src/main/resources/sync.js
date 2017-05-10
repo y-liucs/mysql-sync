1aac0c34a72e468e6b693c71cf5539fd2f54c7b1
Fe <fe@corefine.org>
2017-05-10 10:21:19 +0800

    init

---
+++ src/main/resources/sync.js
@@ -0,0 +1,44 @@
+[
+	{
+		"uic": [
+			{
+				"tableName": "uic_burying_point",
+				"updateable": false
+			}
+		],
+		"trans": [
+			{
+				"tableName": "plat_ranking_history",
+				"updateable": false
+			},
+			{
+				"tableName": "plat_wheel_user_play_record",
+				"updateable": false
+			},
+			{
+				"tableName": "quoits_user_play_record",
+				"updateable": false
+			},
+			{
+				"tableName": "trans_account",
+				"updateable": false
+			},
+			{
+				"tableName": "trans_change_note",
+				"updateable": true
+			},
+			{
+				"tableName": "trans_convert",
+				"updateable": false
+			},
+			{
+				"tableName": "trans_fragment_change_log",
+				"updateable": false
+			},
+			{
+				"tableName": "trans_fragment_convert_record",
+				"updateable": true
+			}
+		]
+	}
+]
