c2addf9c6fabb8217d5092ca9c068d4ff5c0e51a
刘成胜 <liuchengsheng@jddfun.com>
2017-09-04 20:28:47 +0800

    mycat同步配置

--- src/main/resources/newToMycatsync.js
+++ src/main/resources/newToMycatsync.js
@@ -1,8 +1,24 @@
 [
 
 	{
+		"src" : "mycattrans.trans_account",
+		"desc" : "trans.trans_account",
+		"update" : true
+	},
+	{
+		"src" : "mycattrans.trans_change_note",
+		"desc" : "trans.trans_change_note",
+		"update" : false
+	},
+	{
 		"src" : "mycatuic.uic_user",
 		"desc" : "uic.uic_user",
 		"update" : true
-	}
+	},
+	{
+		"src" : "mycatuic.uic_burying_point",
+		"desc" : "uic.uic_burying_point",
+		"update" : false
+	} 
+	
 ]
